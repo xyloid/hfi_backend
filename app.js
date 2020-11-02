5a0dda5599dbfca9f4bb710546e578f3b1212798
yilin xu <xu.yilin@husky.neu.edu>
2020-11-02 14:25:42 -0500

    add app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+const recordsRouter = require("./controllers/records");
+
+users = [{ username: "admin", _id: 1234 }];
+
+const cors = require("cors");
+const express = require("express");
+const app = express();
+app.use(cors());
+
+const mongoose = require("mongoose");
+
+const url = `mongodb+srv://userhfi:userhfi@fso2020.q4awj.azure.mongodb.net/hfi?retryWrites=true&w=majority`;
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+
+app.get("/", (request, response) => {
+  response.send("<h1>Backend is running.</h1>");
+});
+
+app.use("/api/data", recordsRouter);
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,32 +1,9 @@
-const recordsRouter = require("./controllers/records");
+const app = require('./app') // the actual Express application
+const http = require('http')
 
-users = [{ username: "admin", _id: 1234 }];
-
-const cors = require("cors");
-const express = require("express");
-const app = express();
-app.use(cors());
-
-const mongoose = require("mongoose");
-
-const url = `mongodb+srv://userhfi:userhfi@fso2020.q4awj.azure.mongodb.net/hfi?retryWrites=true&w=majority`;
-
-mongoose.connect(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-});
-
-
-app.get("/", (request, response) => {
-  response.send("<h1>Backend is running.</h1>");
-});
-
-app.use("/api/data", recordsRouter);
-
+const server = http.createServer(app)
 
 const PORT = 3001;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
