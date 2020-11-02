88bfded3b94e42590a266e166ba3333bc9bb143b
yilin xu <xu.yilin@husky.neu.edu>
2020-11-02 11:01:22 -0500

    install cors to allow requests from all origins

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var data = JSON.parse(rawdata);
 
 console.log(data);
 
+const cors = require("cors");
 const express = require("express");
 const app = express();
+app.use(cors());
 
 app.get("/", (request, response) => {
   response.send("<h1>Backend is running.</h1>");
