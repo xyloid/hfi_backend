95d048f6c37bb47651dfaa03d5d3f331ece21f2c
yilin xu <xu.yilin@husky.neu.edu>
2020-11-02 15:48:35 -0500

    get records with json web token

--- controllers/records.js
+++ controllers/records.js
@@ -1,10 +1,28 @@
+const jwt = require("jsonwebtoken");
+
 const recordsRouter = require("express").Router();
 const Record = require("../models/record");
+const User = require("../models/user");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    // return token
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 recordsRouter.get("/", (request, response) => {
-  Record.find({}).then((notes) => {
-    response.json(notes);
-  });
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  } else {
+    Record.find({}).then((notes) => {
+      response.json(notes);
+    });
+  }
 });
 
 module.exports = recordsRouter;
