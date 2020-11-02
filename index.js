71de94504899e88ce3a861ccb2f9f0995e8f154e
yilin xu <xu.yilin@husky.neu.edu>
2020-11-02 10:55:47 -0500

    return data in HTTP GET response

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const fs = require('fs');
-let rawdata = fs.readFileSync('./data/data.JSON');
+const fs = require("fs");
+let rawdata = fs.readFileSync("./data/data.JSON");
 var data = JSON.parse(rawdata);
 
-console.log(data)
+console.log(data);
+
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("<h1>Backend is running.</h1>");
+});
+
+app.get("/api/data", (request, response) => {
+  response.json(data);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
