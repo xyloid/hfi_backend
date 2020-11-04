11831b77c2857d8c42f193534076c621bd48a072
yilin xu <xu.yilin@husky.neu.edu>
2020-11-03 20:21:44 -0500

    add Procfile

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require('http')
 
 const server = http.createServer(app)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
